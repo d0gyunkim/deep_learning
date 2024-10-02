df9fd44573d1f6e3d7358c0d06b326a856f29f47
d0gyunkim <herry0515@naver.com>
2024-10-02 13:32:46 +0900

    ㅍㄹ

--- logic.py/sigmoid_fuc.py
+++ logic.py/sigmoid_fuc.py
@@ -2,27 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def sigmoid(x):
-    y = 1/(1+np.exp(-x))
-    return y
+    return 1 / (1 + np.exp(-x))
 
 def step_func(x):
-    return np.array(x>0)
+    return np.array(x > 0, dtype=int)
 
-x = np.linspace(-5,5,100)
+def ReLU(x):
+    return np.maximum(0, x)
+
+# 입력 범위
+x = np.linspace(-5, 5, 100)
+
+# 출력 값 계산
 y_sig = sigmoid(x)
 y_step = step_func(x)
+y_relu = ReLU(x)
 
 # 시각화
-import matplotlib.pyplot as plt
-plt.plot(x,y_sig,'b-',label='sigmoid')
-plt.plot(x,y_step,'b-',label='step')
-plt.xlabel('x',fontsize=12)
-plt.ylabel('h(x)',fontsize=12)
+plt.plot(x, y_sig, 'b-', label='Sigmoid')
+plt.plot(x, y_step, 'g-', label='Step')
+plt.plot(x, y_relu, 'r-', label='ReLU')
+
+plt.xlabel('x', fontsize=12)
+plt.ylabel('h(x)', fontsize=12)
 plt.legend()
 plt.show()
-
-
-
-
-
-
---
+++ multidimension_cal/cal_vec.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+vec = np.array([1,2,3,4],float)
+
+print(np.ndim(vec))
+print(vec.shape())
+print(vec[0])
